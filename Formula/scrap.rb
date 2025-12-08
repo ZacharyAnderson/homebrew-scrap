bdcf9864eb252bd76b22c7e2f201297631d11843
Zach Anderson <zach.anderson@datadoghq.com>
2025-12-07 21:51:24 -0500

    correct urls

--- Formula/scrap.rb
+++ Formula/scrap.rb
@@ -1,20 +1,20 @@
 class Scrap < Formula
   desc "Fast, interactive note-taking CLI tool with integrated explorer interface"
-  homepage "https://github.com/zacharyanderson/scrap"
+  homepage "https://github.com/ZacharyAnderson/Scrap"
   version "0.0.2"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zacharyanderson/scrap/releases/download/v0.0.2/scrap-macos-arm64.tar.gz"
+      url "https://github.com/ZacharyAnderson/Scrap/releases/download/v0.0.2/scrap-macos-arm64.tar.gz"
       sha256 "5b795abac4f1c51790505b4711b6ca705340ec385121fdf60b6701e0ef30f4e8"
     else
-      url "https://github.com/zacharyanderson/scrap/releases/download/v0.0.2/scrap-macos-x86_64.tar.gz"
+      url "https://github.com/ZacharyAnderson/Scrap/releases/download/v0.0.2/scrap-macos-x86_64.tar.gz"
       sha256 "e1ff62f3228fe728ee09912792e85ce2a42b0c906e528b1ad58a88add03e402c"
     end
   end
 
   on_linux do
-    url "https://github.com/zacharyanderson/scrap/releases/download/v0.0.2/scrap-linux-x86_64.tar.gz"
+    url "https://github.com/ZacharyAnderson/Scrap/releases/download/v0.0.2/scrap-linux-x86_64.tar.gz"
     sha256 "9cc274cb7ff6b9088843cc64765f14166fec305aeca318cae78f1f1829d4878f"
   end
 
